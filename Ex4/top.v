8edd4790a56e6ba930e7dbbaf2ebb53426b1bd33
Samanyu-T <84709072+Samanyu-T@users.noreply.github.com>
2021-05-26 11:58:38 +0100

    Update top.v

--- Ex4/top.v
+++ Ex4/top.v
@@ -16,3 +16,31 @@
 //  You need to write the whole file.
 //////////////////////////////////////////////////////////////////////////////////
 
+`timescale 1ns / 100ps
+
+module lightcontrol (
+    //Todo: add ports 
+	input clk,rst,button,initial_colour,
+  output reg [2:0]colour
+    );
+                    
+    //Todo: add registers and wires, if needed
+  reg [2:0]colour;
+
+    //Todo: add user logic
+  always @(posedge clk)
+    begin
+   // if the colour is out of bounds or rst is on then set to 1
+    if ((colour == 3'b000) + (colour == 3'b111) + (rst == 1))
+      colour = 3'b001;
+    else
+      begin
+   // if button is on then increase the colour
+       if (button == 1)
+         colour = colour + 1;
+       else
+    // if the button is off then keep the colour constant
+         colour = colour;
+      end
+endmodule
+ 
