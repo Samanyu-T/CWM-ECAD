b11f875ee40561666b6a65e4d73dd71cccf778a6
Samanyu-T <84709072+Samanyu-T@users.noreply.github.com>
2021-05-26 16:48:39 +0100

    Update top.v

--- Ex5/top.v
+++ Ex5/top.v
@@ -13,3 +13,35 @@
 //           heating, cooling
 //////////////////////////////////////////////////////////////////////////////////
 
+`timescale 1ns / 100ps
+
+module tempmonitor (
+    //Todo: add ports 
+  input clk,temperature[4:0],
+	output heating, cooling
+    );
+  wire heating
+  wire cooling
+  //apply the conditions to determine whether cooling or heating is required
+  initial 
+    begin 
+      //if too cold apply heating
+      if (temperature <= 5'd18)
+        begin
+          assign heating = 1;
+          assign cooling = 0;
+        end
+      // if too hot apply cooling
+      else if (temperature >= 5'd22)
+        begin
+          assign heating = 0;
+          assign cooling = 1;
+        end
+      // if neither do nothing
+      else
+        begin 
+          assign heating = 0;
+          assign cooling = 0;
+        end 
+    end
+endmodule 
