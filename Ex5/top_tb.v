1f6855520e01bc40a22e36247aebfc5eb6bcf731
Samanyu-T <84709072+Samanyu-T@users.noreply.github.com>
2021-05-27 11:24:59 +0100

    Update top_tb.v

--- Ex5/top_tb.v
+++ Ex5/top_tb.v
@@ -1,12 +1,11 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Test bench for Exercise #5 - Air Conditioning
-// Student Name: 
+// Student Name:
 // Date: 
 //
 // Description: A testbench module to test Ex5 - Air Conditioning
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
-
 `timescale 1ns / 100ps
 
 module top_tb(
@@ -17,12 +16,12 @@ parameter clk_time = 10; //set the clock period
 
 //Todo: Registers and wires
   reg clk, temperature[4:0];
-    wires initial_cooling, initial_heating
+  reg initial_cooling, initial_heating;
   
   //Intialise the clock signal
- initial begin 
+ initial 
+  begin 
    clk=0;
-   cycle_count = 0;
    forever
    begin
      #(clk_time/2) clk=~clk;
@@ -32,17 +31,17 @@ parameter clk_time = 10; //set the clock period
   //Apply a given set of initial conditions
   initial 
       begin
-        assign initial_cooling = 1;
-        assign initial_heating = 0;
-        assign temperature = 5'd21;
+         initial_cooling = 1;
+         initial_heating = 0;
+         temperature = 5'd21;
+	#50;
       end 
   initial
       $display("The temperature is %d and the heating is %d and the cooling is %d",temperature,heating,cooling);
    
-    monitor top(.clk(clk), .temperature(temperature), .heating(heating),.cooling(cooling),.initiail_heating(initial_heating), .intial_cooling(initial_cooling));
+    tempmonitor top(.clk(clk), .temperature(temperature), .heating(heating), .cooling(cooling),
+		.initial_heating(initial_heating), .initial_cooling(initial_cooling) );
 endmodule
                    
       
-  
-  
   
