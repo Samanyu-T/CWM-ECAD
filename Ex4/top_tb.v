1556b29ab4debd2cb2230c74125a57bbc2f37d80
Samanyu-T <84709072+Samanyu-T@users.noreply.github.com>
2021-05-28 14:20:55 +0100

    Update top_tb.v
    Finished - Used Simgui to properly monitor the colour signal

--- Ex4/top_tb.v
+++ Ex4/top_tb.v
@@ -16,16 +16,19 @@ module top_tb(
 parameter clk_time = 10; //set the clock period
 
 //Todo: Registers and wires
-reg clk, rst, button, cycle_count ;
+reg clk;
+reg rst;
+reg button;
+reg [3:0] cycle_count ;
+wire [2:0] colour;
 
 //Todo: Clock generation
 initial begin 
- clk=1'b0;
+ #(clk_time/2)
+ clk=1;
  forever
    begin
    #(clk_time/2) clk=~clk;
-  //Increment the counter every clock cycle
-   cycle_count = cycle_count + 1;
    end
 end
   
@@ -35,16 +38,20 @@ initial begin
   button = 1; 
   rst = 0;
   cycle_count = 4'h0;
-
+  #(clk_time)
 forever
   begin
+// To test the system accurately I used the simgui to monitor the values with respect to the clock signal
+    #(clk_time)
+    //Increment the counter every clock cycle
+    cycle_count = cycle_count + 4'h1;
     if (cycle_count == 4'hF)
       begin
 //display the final colour
       $display("Colour is %d",colour);
       $finish;
       end
-//Turn off the button at the mddle 
+//Turn off the button at the middle 
     else if (cycle_count == 4'hA)
         begin
 //display the intermediary colour to check the looping
