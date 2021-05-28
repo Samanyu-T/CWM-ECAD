b7f608d210650df8c0e4e422c3233ad303bc5c2f
Samanyu-T <84709072+Samanyu-T@users.noreply.github.com>
2021-05-28 17:39:25 +0100

    Not complete at all, but some pseudocode

--- Ex8/top_tb.v
+++ Ex8/top_tb.v
@@ -7,3 +7,8 @@
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
 
+//I don't have enough time to fully write this so:
+//Make registers to store the inputs to top.v, i.e reg temperature_0 = 1'b0 and so on 
+//Not sure on how to input the clock signals 
+//Once this is intialise simply use the same test criterion as in Excercise 5
+//So send 1 set of data and check if the output is reasonable.
