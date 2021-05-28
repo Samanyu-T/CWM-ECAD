2e157be03a520de7bf4a553cf73c65ffc9a22adb
Samanyu-T <84709072+Samanyu-T@users.noreply.github.com>
2021-05-28 17:40:07 +0100

    Update top.v

--- Ex8/top.v
+++ Ex8/top.v
@@ -16,6 +16,14 @@
 module top(
     input clk_p,
     input clk_n,
+    input rst_n,
+    input temperature_0,
+    input temperature_1,
+    input temperature_2,
+    input temperature_3,
+    input temperature_4,
+    output heating,
+    output cooling
      //Todo: add all other ports besides clk_n and clk_p 
    );
     
@@ -36,6 +44,17 @@ module top(
 	.O  (clk)
       );
 
-//Add logic here
 
+//Add logic here
+	wire [4:0]temperature;
+	assign temperature[0] = temperature_0;
+	assign temperature[1] = temperature_1;
+	assign temperature[2] = temperature_2;
+	assign temperature[3] = temperature_3;
+	assign temperature[4] = temperature_4;
+
+  //apply the conditions to determine whether cooling or heating is required
+	tempmonitor topEx5(.temperature(temperature), .clk(clk), .heating(heating), .cooling(cooling));
+		   
+	    
 endmodule
